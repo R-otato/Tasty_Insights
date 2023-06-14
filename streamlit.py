52b425c75248d56a0a674c1aa9e235ce571c8ada
Ryan Liam <ryanliampy@gmail.com>
2023-06-14 10:38:31 +0800

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,22 @@
+#--Team--
+# Tutorial Group: 	T01 Group 4 <br>
+
+# Student Name 1:	Ryan Liam Poon Yang<br>
+# Student Number: 	S10222131E <br>
+# Student Name 2:	Teh Zhi Xian<br>
+# Student Number: 	S10221851J<br>
+# Student Name 3:	Chuah Kai Yi<br>
+# Student Number: 	S10219179E<br>
+# Student Name 4:	Don Sukkram<br>
+# Student Number: 	S10223354J<br>
+# Student Name 5:	Darryl Koh<br>
+# Student Number: 	S10221893J<br>
+
+#--Import statements--
+import streamlit
+import pandas as pd
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+#--Import statements--
