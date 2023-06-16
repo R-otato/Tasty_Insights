e728e26fc0f1c6c9b306a9a655671d28c98a03fa
Ryan Liam <ryanliampy@gmail.com>
2023-06-16 21:03:30 +0800

    remove snowflake

--- streamlit_pages/1_ChurnPrediction.py
+++ streamlit_pages/1_ChurnPrediction.py
@@ -16,7 +16,6 @@
 import streamlit as st
 import pandas as pd
 import requests
-import snowflake.connector
 from urllib.error import URLError
 import numpy as np
 import time
--- streamlit_pages/Home.py
+++ streamlit_pages/Home.py
@@ -16,7 +16,6 @@
 import streamlit as st
 import pandas as pd
 import requests
-import snowflake.connector
 from urllib.error import URLError
 
 #--Page 1--
