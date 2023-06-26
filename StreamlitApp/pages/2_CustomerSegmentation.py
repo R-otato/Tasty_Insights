46f450c3a2b188af7f7bb3fff13d6d208104c764
Poon Yang Ryan Liam <ryanliampy@gmail.com>
2023-06-26 15:10:31 +0800

    edit

---
+++ StreamlitApp/pages/2_CustomerSegmentation.py
@@ -0,0 +1,15 @@
+#--Import statements--
+import streamlit as st
+import pandas as pd
+import requests
+import numpy as np
+import joblib 
+import time
+from snowflake.snowpark import Session
+import json
+from snowflake.snowpark.functions import call_udf, col
+import snowflake.snowpark.types as T
+from cachetools import cached
+
+st.set_page_config(page_title="Customer Segmentation", page_icon="📈")
+st.markdown('Hello')
