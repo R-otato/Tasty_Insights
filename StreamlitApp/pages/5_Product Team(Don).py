2c05e4ebee028c95463d9ef29b89db4519d08943
DonSukkram <S10223354@connect.np.edu.sg>
2023-07-13 13:35:15 +0800

    Added model prediction process and outcome to streamlit file
    added data transformation, added model prediction, and the output of the model prediction in the streamlit file

--- StreamlitApp/pages/5_Product Team(Don).py
+++ StreamlitApp/pages/5_Product Team(Don).py
@@ -1,9 +1,37 @@
 #--Import statements--
 import streamlit as st
 import pandas as pd
+import joblib 
+
+#--Functions--
+
+# Function: pipline
+# the purpose of this function is to carry out the necessary transformations on the data provided by the user so that it can be fed into the machine learning model for prediction
+def pipeline(data):
+    # Load the necessary transformations
+    windsorizer_iqr = joblib.load("assets/windsorizer_iqr.jbl")
+    windsorizer_gau = joblib.load("assets/windsorizer_gau.jbl")
+    yjt = joblib.load("assets/yjt.jbl")
+    ohe_enc = joblib.load("assets/ohe_enc.jbl")
+    minMaxScaler = joblib.load("assets/minMaxScaler.jbl")
+
+    # Apply the transformations to the data
+    data = windsorizer_iqr.transform(data)  # Apply IQR Windsorization
+    data = windsorizer_gau.transform(data)  # Apply Gaussian Windsorization
+    data = yjt.transform(data)  # Apply Yeo-Johnson Transformation
+    data = ohe_enc.transform(data)  # Apply One-Hot Encoding
+    data.columns = data.columns.str.upper()
+    data[data.columns] = minMaxScaler.transform(data[data.columns])  # Apply Min-Max Scaling
+    
+    return data
 
 
-# Page Layout
+
+
+
+#################
+### MAIN CODE ### 
+#################
 
 # Page Title
 st.markdown("# Product")
@@ -18,7 +46,8 @@ with st.expander("How to Use This Page"):
     st.write('2. View the model\'s predictions')
     st.write('3. Analyse the visualisations below to gain insights on how to reduce customer churn from a product standpoint')
 
-# Input data
+
+# SECTION: INPUT DATA 
 ## File Upload section
 st.markdown("## Input Data")
 uploaded_files = st.file_uploader('Upload your file(s)', accept_multiple_files=True)
@@ -38,3 +67,24 @@ else:
     st.info("Using the last updated data of the members in United States. Upload a file above to use your own data!")
     #df=pd.read_csv('StreamlitApp/assets/without_transformation.csv')
     df=pd.read_csv('assets/without_transformation.csv')
+    
+# Preparing the data for prediction
+## Removing Customer ID column
+customer_id = df.pop("CUSTOMER_ID")
+#Get categoorical columns
+demo_df=df[['GENDER','MARITAL_STATUS','CITY','CHILDREN_COUNT','AGE']]
+beha_df=df.loc[:, ~df.columns.isin(['GENDER','MARITAL_STATUS','CITY','CHILDREN_COUNT','AGE'])]
+
+df=pipeline(df)
+
+with st.expander("Cleaned and Transformed Data"):
+    st.write(df)
+    
+# MODEL FOR PREDICTION
+model = joblib.load("assets/churn-prediction-model.jbl")
+predictions= pd.DataFrame(model.predict(df),columns=['CHURNED'])
+demo_df = pd.concat([demo_df, predictions], axis=1)
+beha_df = pd.concat([beha_df, predictions], axis=1)
+data=pd.concat([customer_id, predictions], axis=1)
+
+st.write(data)
