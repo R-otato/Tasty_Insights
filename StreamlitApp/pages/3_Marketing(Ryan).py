aa91d21a91df6d015e71cfac0d95446edecfe081
Ryan Liam <ryanliampy@gmail.com>
2023-07-19 20:04:27 +0800

    Added filter and export function

--- StreamlitApp/pages/3_Marketing(Ryan).py
+++ StreamlitApp/pages/3_Marketing(Ryan).py
@@ -1,14 +1,16 @@
 #--Import statements--
 import streamlit as st
 import pandas as pd
-from xgboost import XGBClassifier
-import requests
 import numpy as np
 import joblib 
-import time
-import json
 from cachetools import cached
 import plotly.express as px
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
 
 
 #################
@@ -59,6 +61,85 @@ def pipeline(data):
 def load_model(model_path: str) -> object:
     model = joblib.load(model_path)
     return model    
+
+#Convert dataframe to csv 
+def convert_df(df):
+   return df.to_csv(index=False).encode('utf-8')
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Add filters")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
 
 #################
 ### MAIN CODE ### 
@@ -125,9 +206,22 @@ def main() -> None:
 
     st.markdown("## Churn Prediction")
     st.dataframe(data.value_counts('CHURNED'))
-    
-    st.markdown("## Complete Table with model output")
-    st.write(data)
+
+    st.markdown("## Overall Table")
+    filtered_data=filter_dataframe(data)
+    st.dataframe(filtered_data)
+
+    #Allow user to download dataframe for further analysis
+    st.header('**Export results ✨**')
+    st.write("_Finally you can export the resulting table after Clustering and Churn Prediction._")
+    csv = convert_df(filtered_data)
+    st.download_button(
+    "Press to Download",
+    csv,
+    "file.csv",
+    "text/csv",
+    key='download-csv'
+    )
 
  
 ###########################
