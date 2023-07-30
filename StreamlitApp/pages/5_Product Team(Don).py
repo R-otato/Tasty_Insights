b7bca89a5078f68e82bf004a78951898fc0d6c34
DonSukkram <S10223354@connect.np.edu.sg>
2023-07-30 17:58:18 +0800

    Clean up code

--- StreamlitApp/pages/5_Product Team(Don).py
+++ StreamlitApp/pages/5_Product Team(Don).py
@@ -16,7 +16,46 @@ def my_encode_units(x):
     if x >= 1:
         return 1
 
+# Function: get_item_bundles_unformatted(df)
+# the purpose of this function is to search for bundles within the provided dataset
+def get_item_bundles_unformatted(df):
+    # Data preparation for apriori
+    ## get table format for apriori algo
+    mybasket = df.groupby(["ORDER_ID", "MENU_ITEM_ID"])["QUANTITY"].sum().unstack().reset_index().fillna(0).set_index("ORDER_ID")
 
+    ## convert all positive values to 1 and everything else to 0
+    my_basket_sets = mybasket.applymap(my_encode_units)
+
+    # Apriori Model in action
+    ## load apriori model
+    apriori_model = joblib.load("assets/apriori_algo_product.joblib")
+
+    ## generating rules
+    my_rules = association_rules(apriori_model, metric="lift", min_threshold=14)
+
+    # Fix formatting issue
+    ## Convert frozensets to strings in the antecedents and consequents columns
+    my_rules['antecedents'] = my_rules['antecedents'].apply(lambda x: tuple(x))
+    my_rules['consequents'] = my_rules['consequents'].apply(lambda x: tuple(x))
+
+    ## Convert 'frozenset' objects to strings in the 'antecedents' and 'consequents' columns
+    my_rules['antecedents'] = my_rules['antecedents'].apply(lambda x: ', '.join(map(str, x)))
+    my_rules['consequents'] = my_rules['consequents'].apply(lambda x: ', '.join(map(str, x)))
+
+    # Remove duplicate bundles
+    ## Create a new column with unique itemsets
+    my_rules['itemset'] = my_rules[['antecedents', 'consequents']].apply(lambda x: frozenset(x), axis=1)
+
+    ## Drop duplicates based on the 'itemset' column
+    my_rules.drop_duplicates(subset='itemset', keep='first', inplace=True)
+
+    ## Drop the 'itemset' column (optional)
+    my_rules.drop(columns='itemset', inplace=True)
+
+    ## Reset the index if needed
+    my_rules.reset_index(drop=True, inplace=True)
+
+    return my_rules
 
 
 #####################
@@ -58,41 +97,9 @@ else:
 
 st.write(df)
 
-# Data preparation for apriori
-## get table format for apriori algo
-mybasket = df.groupby(["ORDER_ID", "MENU_ITEM_ID"])["QUANTITY"].sum().unstack().reset_index().fillna(0).set_index("ORDER_ID")
 
-## convert all positive values to 1 and everything else to 0
-my_basket_sets = mybasket.applymap(my_encode_units)
-
-# Apriori Model in action
-## load apriori model
-apriori_model = joblib.load("assets/apriori_algo_product.joblib")
-
-## generating rules
-my_rules = association_rules(apriori_model, metric="lift", min_threshold=14)
-
-# Fix formatting issue
-## Convert frozensets to strings in the antecedents and consequents columns
-my_rules['antecedents'] = my_rules['antecedents'].apply(lambda x: tuple(x))
-my_rules['consequents'] = my_rules['consequents'].apply(lambda x: tuple(x))
-
-## Convert 'frozenset' objects to strings in the 'antecedents' and 'consequents' columns
-my_rules['antecedents'] = my_rules['antecedents'].apply(lambda x: ', '.join(map(str, x)))
-my_rules['consequents'] = my_rules['consequents'].apply(lambda x: ', '.join(map(str, x)))
-
-# Remove duplicate bundles
-## Create a new column with unique itemsets
-my_rules['itemset'] = my_rules[['antecedents', 'consequents']].apply(lambda x: frozenset(x), axis=1)
-
-## Drop duplicates based on the 'itemset' column
-my_rules.drop_duplicates(subset='itemset', keep='first', inplace=True)
-
-## Drop the 'itemset' column (optional)
-my_rules.drop(columns='itemset', inplace=True)
-
-## Reset the index if needed
-my_rules.reset_index(drop=True, inplace=True)
+# retrieve bundles found by apriori
+my_rules = get_item_bundles_unformatted(df)
 
 st.write(my_rules)
 
