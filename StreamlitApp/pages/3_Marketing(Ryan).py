139fecc963a6bfbc28388e6fb723330d8d8104d4
Ryan Liam <ryanliampy@gmail.com>
2023-08-01 22:31:35 +0800

    test

--- StreamlitApp/pages/3_Marketing(Ryan).py
+++ StreamlitApp/pages/3_Marketing(Ryan).py
@@ -22,13 +22,40 @@ from datetime import datetime
 
 #Note to teammates: Copy the functions below if you want to transform the data to perform Kmeans clustering and Churn prediction
 #Else if you want to perform Churn prediction just edit the code by removing data_kmeans.
-
-def pipeline(data):
+def kmeans_pipeline(data):
     ## Make a copy first
     data=data.copy()
 
     ## Removing columns not used in transformation
-    cols_Not_Involved=['CUSTOMER_ID','FREQUENT_MENU_ITEMS','FREQUENT_MENU_TYPE','FREQUENT_TRUCK_BRAND','PREFERRED_TIME_OF_DAY','PROFIT','PROFIT_MARGIN(%)']
+    cols_Not_Involved=['CUSTOMER_ID','FREQUENT_MENU_ITEMS','FREQUENT_MENU_TYPE','FREQUENT_TRUCK_BRAND','PREFERRED_TIME_OF_DAY','PROFIT','PROFIT_MARGIN(%)','AVG_SALES_ORDER','TENURE_MONTHS']
+    not_Involved=data[cols_Not_Involved]
+    data.drop(cols_Not_Involved,axis=1,inplace=True)
+
+    # Load the necessary transformations
+    windsorizer_iqr = joblib.load("assets/windsorizer_iqr.jbl")
+    windsorizer_gau = joblib.load("assets/windsorizer_gau.jbl")
+    kmeansMinMaxScaler=joblib.load("assets/kmeans_scaling.jbl")
+
+    # Apply the transformations to the data
+    #Both models table transformation
+    data = windsorizer_iqr.transform(data)  # Apply IQR Windsorization
+    data = windsorizer_gau.transform(data)  # Apply Gaussian Windsorization
+    cols_to_scale=['RECENCY','FREQUENCY','MONETARY']
+    data_kmeans=data[cols_to_scale].copy() # For our Kmeans model, it does not include any yeo johnson transformation
+    data_kmeans[cols_to_scale] = kmeansMinMaxScaler.transform(data_kmeans[cols_to_scale])  # Apply Min-Max Scaling for Kmeans
+
+    #Concat Customer ID back
+    data_kmeans=pd.concat([not_Involved, data_kmeans], axis=1)
+
+    return data_kmeans
+
+
+def churn_pipeline(data):
+    ## Make a copy first
+    data=data.copy()
+
+    ## Removing columns not used in transformation
+    cols_Not_Involved=['CUSTOMER_ID','FREQUENT_MENU_ITEMS','FREQUENT_MENU_TYPE','FREQUENT_TRUCK_BRAND','PREFERRED_TIME_OF_DAY','PROFIT','PROFIT_MARGIN(%)','AVG_SALES_ORDER','TENURE_MONTHS']
     not_Involved=data[cols_Not_Involved]
     data.drop(cols_Not_Involved,axis=1,inplace=True)
 
@@ -38,19 +65,10 @@ def pipeline(data):
     yjt = joblib.load("assets/yjt.jbl")
     ohe_enc = joblib.load("assets/ohe_enc.jbl")
     minMaxScaler = joblib.load("assets/minMaxScaler.jbl")
-    kmeansMinMaxScaler=joblib.load("assets/kmeans_scaling.jbl")
 
     # Apply the transformations to the data
-    #Both models table transformation
     data = windsorizer_iqr.transform(data)  # Apply IQR Windsorization
     data = windsorizer_gau.transform(data)  # Apply Gaussian Windsorization
-
-    #KMeans table tranformation
-    cols_to_scale=['RECENCY','FREQUENCY','MONETARY']
-    data_kmeans=data[cols_to_scale].copy() # For our Kmeans model, it does not include any yeo johnson transformation
-    data_kmeans[cols_to_scale] = kmeansMinMaxScaler.transform(data_kmeans[cols_to_scale])  # Apply Min-Max Scaling for Kmeans
-
-    #Churn prediction table transformation
     data = yjt.transform(data)  # Apply Yeo-Johnson Transformation
     data = ohe_enc.transform(data)  # Apply One-Hot Encoding
     data.columns = data.columns.str.upper() #Normalize naming convention
@@ -58,9 +76,27 @@ def pipeline(data):
     
     #Concat Customer ID back
     data=pd.concat([not_Involved, data], axis=1)
-    data_kmeans=pd.concat([not_Involved, data_kmeans], axis=1)
 
-    return data,data_kmeans
+    return data
+
+def sales_pipeline(data):
+    ## Make a copy first
+    data=data.copy()
+
+    ## Filter columns
+    not_Involved=data[['CUSTOMER_ID','MONETARY']]
+    data=data[['CUSTOMER_ID','MONETARY','FREQUENCY','AVG_SALES_ORDER','TENURE_MONTHS']]
+
+    # Load the necessary transformations
+    windsorizer_gau = joblib.load("assets/memb_sales_win_gau.jbl")
+    minMaxScaler = joblib.load("assets/memb_sales_scale.jbl")
+
+    # Apply the transformations to the data
+    data = windsorizer_gau.transform(data)  # Apply Gaussian Windsorization
+    data[['FREQUENCY','AVG_SALES_ORDER','TENURE_MONTHS']] = minMaxScaler.transform(data[['FREQUENCY','AVG_SALES_ORDER','TENURE_MONTHS']])  # Apply Min-Max Scaling
+
+    return data
+
 
 #Load model - To teammates: you can just copy this entirely
 def load_model(model_path: str) -> object:
@@ -92,8 +128,8 @@ def main() -> None:
     st.markdown("## High Level Goals")
     st.write("""As stated in our homepage, our team is dedicated to assisting Tasty Bytes in achieving its ambitious goals over the next 5 years. 
                 In particular, we aim to assist helping Tasty Bytes achieve a remarkable 25% Year-Over-Year increase. This page is designed 
-                exclusively for the marketing team, showcasing our data-driven 
-                approach to empower and elevate your marketing strategies, ultimately driving significant sales growth.""")
+                exclusively for the marketing team, showcasing our data-driven approach to empower and elevate your marketing strategies, 
+             ultimately driving significant sales growth.""")
     
     with st.expander("How to Use This Page"):
         #Going to add some stuff here 
@@ -131,7 +167,8 @@ def main() -> None:
         st.write(df.head(10))
 
     # Run pipeline
-    clean_df,kmeans_df=pipeline(df)
+    clean_df=churn_pipeline(df)
+    kmeans_df=kmeans_pipeline(df)
 
     with st.expander("Cleaned and Transformed Data"):
         st.write(clean_df.head(10))
@@ -139,9 +176,10 @@ def main() -> None:
     # Setup: Model loading
     churn_model = load_model("assets/churn-prediction-model.jbl")
     seg_clf_model = load_model("assets/models/segment_classifier.jbl")
+    sales_model=load_model("assets/models/memb_sales_pred.jbl")
 
     # Setup: Get predictions
-    cols_to_ignore=['CUSTOMER_ID','FREQUENT_MENU_ITEMS','FREQUENT_MENU_TYPE','FREQUENT_TRUCK_BRAND','PREFERRED_TIME_OF_DAY','PROFIT','PROFIT_MARGIN(%)']
+    cols_to_ignore=['CUSTOMER_ID','FREQUENT_MENU_ITEMS','FREQUENT_MENU_TYPE','FREQUENT_TRUCK_BRAND','PREFERRED_TIME_OF_DAY','PROFIT','PROFIT_MARGIN(%)','AVG_SALES_ORDER','TENURE_MONTHS']
     churn_pred= pd.DataFrame(churn_model.predict(clean_df.drop(cols_to_ignore,axis=1)),columns=['CHURNED'])
     cluster_pred=pd.DataFrame(seg_clf_model.predict(kmeans_df.drop(cols_to_ignore,axis=1)),columns=['CLUSTER'])
     
@@ -170,8 +208,6 @@ def main() -> None:
     selected_Churn= st.multiselect("Filter by Churn:",churn_Options, default=['All'])
     filtered_data=filter(selected_Churn,'CHURNED',filtered_data)
     
-    #Summary
-    st.markdown("### Summary")
     # Number of members of each cluster
     cluster_counts = filtered_data.groupby('CLUSTER').size().reset_index(name='Number of Members')
     st.dataframe(cluster_counts, hide_index=True)
@@ -180,58 +216,78 @@ def main() -> None:
     churn_counts = filtered_data.groupby('CHURNED').size().reset_index(name='Number of Members')
     st.dataframe(churn_counts, hide_index=True)
 
-    # Metrics table
-    st.markdown("### Member's Metric")
-    ## Display table
-    metric_df=filtered_data[['CUSTOMER_ID','RECENCY','FREQUENCY','MONETARY','LENGTH_OF_RELATIONSHIP','PROFIT','PROFIT_MARGIN(%)']]
-    st.dataframe(metric_df, hide_index=True)
+    # Display forecasted sales
+    st.write('### Member Forecasted Sales')
+    forecast_months = st.slider('Months for forecast:', 1, 12, 1)
+    estimated_frequency = int(st.text_input("Expected purchases in period:", 1))
+
+    sales_model_input = filtered_data[['CUSTOMER_ID', 'FREQUENCY', 'AVG_SALES_ORDER', 'TENURE_MONTHS','MONETARY']]
+
+    sales_model_input['FREQUENCY'] = sales_model_input['FREQUENCY'] + estimated_frequency
+    sales_model_input['TENURE_MONTHS'] = sales_model_input['TENURE_MONTHS'] + forecast_months
+
+    sales_clean=sales_pipeline(sales_model_input)
+    monetary_pred= pd.DataFrame(sales_model.predict(sales_clean.drop(['CUSTOMER_ID','MONETARY'],axis=1)),columns=['FORECAST_MONETARY'])
+
+    sales_model_input['FORECAST_MONETARY']=round(monetary_pred['FORECAST_MONETARY'],2)
+    sales_model_input['FORECAST_SALES']=round(sales_model_input['FORECAST_MONETARY']-sales_model_input['MONETARY'],2)
+
+    st.write(sales_model_input)
+    st.metric('Forecasted Sales', f"${round(sales_model_input['FORECAST_SALES'].sum(),2)}")
 
 
-    # Demographic table
-    st.markdown("### Member's Demographic")
-    ## Clean up columns
-    filtered_data['CHILDREN_COUNT'] = filtered_data['CHILDREN_COUNT'].map({
-    '0': "No",
-    '1': "Yes",
-    '2': "Yes",
-    '3': "Yes",
-    '4': "Yes",
-    '5+': "Yes",
-    'Undisclosed':'Undisclosed'})
-    filtered_data.rename({'CHILDREN_COUNT':'HAVE_CHILDREN'},inplace=True,errors='ignore',axis=1)
-    ## Display table
-    demo_df=filtered_data[['CUSTOMER_ID','GENDER','MARITAL_STATUS','CITY','HAVE_CHILDREN','AGE']]
-    st.dataframe(demo_df, hide_index=True)
+    # # Metrics table
+    # st.markdown("### Member's Metric")
+    # ## Display table
+    # metric_df=filtered_data[['CUSTOMER_ID','RECENCY','FREQUENCY','MONETARY','LENGTH_OF_RELATIONSHIP','PROFIT','PROFIT_MARGIN(%)']]
+    # st.dataframe(metric_df, hide_index=True)
 
-    # Behavioral table
-    st.markdown("### Member's Behaviour")
+
+    # # Demographic table
+    # st.markdown("### Member's Demographic")
+    # ## Clean up columns
+    # filtered_data['CHILDREN_COUNT'] = filtered_data['CHILDREN_COUNT'].map({
+    # '0': "No",
+    # '1': "Yes",
+    # '2': "Yes",
+    # '3': "Yes",
+    # '4': "Yes",
+    # '5+': "Yes",
+    # 'Undisclosed':'Undisclosed'})
+    # filtered_data.rename({'CHILDREN_COUNT':'HAVE_CHILDREN'},inplace=True,errors='ignore',axis=1)
+    # ## Display table
+    # demo_df=filtered_data[['CUSTOMER_ID','GENDER','MARITAL_STATUS','CITY','HAVE_CHILDREN','AGE']]
+    # st.dataframe(demo_df, hide_index=True)
+
+    # # Behavioral table
+    # st.markdown("### Member's Behaviour")
     
-    ## Clean up columns
-    # # Convert 'LENGTH_OF_RELATIONSHIP' from days to years (with decimal point)
-    # filtered_data['LENGTH_OF_RELATIONSHIP_YEARS'] = filtered_data['LENGTH_OF_RELATIONSHIP'] / 365.25
+    # ## Clean up columns
+    # # # Convert 'LENGTH_OF_RELATIONSHIP' from days to years (with decimal point)
+    # # filtered_data['LENGTH_OF_RELATIONSHIP_YEARS'] = filtered_data['LENGTH_OF_RELATIONSHIP'] / 365.25
 
-    # # Rename the new column to indicate it represents relationship duration in decimal years
-    # filtered_data.rename(columns={'LENGTH_OF_RELATIONSHIP': 'LENGTH_OF_RELATIONSHIP_DAYS'}, inplace=True)
+    # # # Rename the new column to indicate it represents relationship duration in decimal years
+    # # filtered_data.rename(columns={'LENGTH_OF_RELATIONSHIP': 'LENGTH_OF_RELATIONSHIP_DAYS'}, inplace=True)
 
-    ## Display table
-    beha_df=filtered_data[['CUSTOMER_ID','FREQUENT_MENU_ITEMS','FREQUENT_MENU_TYPE','FREQUENT_TRUCK_BRAND','PREFERRED_TIME_OF_DAY']]
-    st.dataframe(beha_df, hide_index=True)
+    # ## Display table
+    # beha_df=filtered_data[['CUSTOMER_ID','FREQUENT_MENU_ITEMS','FREQUENT_MENU_TYPE','FREQUENT_TRUCK_BRAND','PREFERRED_TIME_OF_DAY']]
+    # st.dataframe(beha_df, hide_index=True)
 
-    # Overall Table
-    st.markdown("### Overall Table")
-    st.dataframe(filtered_data, hide_index=True)
+    # # Overall Table
+    # st.markdown("### Overall Table")
+    # st.dataframe(filtered_data, hide_index=True)
 
-    #Allow user to download dataframe for further analysis
-    st.header('**Export results ✨**')
-    st.write("_Finally you can export the resulting table after Clustering and Churn Prediction._")
-    csv = convert_df(filtered_data)
-    st.download_button(
-    "Press to Download",
-    csv,
-    "marketing.csv",
-    "text/csv",
-    key='download-csv'
-    )
+    # #Allow user to download dataframe for further analysis
+    # st.header('**Export results ✨**')
+    # st.write("_Finally you can export the resulting table after Clustering and Churn Prediction._")
+    # csv = convert_df(filtered_data)
+    # st.download_button(
+    # "Press to Download",
+    # csv,
+    # "marketing.csv",
+    # "text/csv",
+    # key='download-csv'
+    # )
 
  
 ###########################
